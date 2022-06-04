2c1e5cd80bca1a2f56c4b5aa5732b6342dbecb57
Brandon Woodford <ivegot@norto.dev>
2022-06-04 18:42:03 -0500

    Create incomplete version of project for desktop

--- main.go
+++ main.go
@@ -7,17 +7,42 @@ import (
 	"net"
 )
 
+const (
+	INPUT                             = "10 "
+	INPUT_SENSITIVE                   = "11 "
+	SUCCESS                           = "20 "
+	REDIRECT_TEMPORARY                = "30 "
+	REDIRECT_PERMANENT                = "31 "
+	TEMPORARY_FAILURE                 = "40 "
+	SERVER_UNAVAILABLE                = "41 "
+	CGI_ERROR                         = "42 "
+	PROXY_ERROR                       = "43 "
+	SLOW_DOWN                         = "44 "
+	PERMANENT_FAILURE                 = "50 "
+	NOT_FOUND                         = "51 "
+	GONE                              = "52 "
+	PROXY_REQUEST_REFUSED             = "53 "
+	BAD_REQUEST                       = "59 "
+	CLIENT_CERTIFICATE_REQUIRED       = "60 "
+	CLIENT_CERTIFICATE_NOT_AUTHORISED = "61 "
+	CLIENT_CERTIFICATE_NOT_VALID      = "62 "
+)
+
 func main() {
 	log.SetFlags(log.Lshortfile)
 
-	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	cer, err := tls.LoadX509KeyPair("server.pem", "server.key")
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	ln, err := tls.Listen("tcp", ":1967", config)
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cer},
+		MinVersion:   tls.VersionTLS13,
+	}
+
+	ln, err := tls.Listen("tcp", ":1965", config)
 	if err != nil {
 		log.Println(err)
 		return
@@ -36,20 +61,23 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	r := bufio.NewReader(conn)
-	for {
-		msg, err := r.ReadString('\n')
-		if err != nil {
-			log.Println(err)
-			return
-		}
+	r := bufio.NewScanner(conn)
 
-		println(msg)
+	resp := handleRequest(msg, err)
+	n, err := conn.Write(resp)
 
-		n, err := conn.Write([]byte("world\n"))
-		if err != nil {
-			log.Println(n, err)
-			return
-		}
+	if err != nil {
+		log.Println(n, err)
+		return
 	}
+}
+
+func handleRequest(req string, err error) []byte {
+	resp := ""
+	if err != nil {
+		resp += BAD_REQUEST
+	} else {
+		// deconstruct request
+	}
+	return []byte(resp)
 }
