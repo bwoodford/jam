73389ac2f7895220dff92243358da21dafe2cc8d
Brandon Woodford <ivegot@norto.dev>
2022-06-05 18:19:43 -0500

    testing: Add failure tests for readRequestLines
    Also updated the passing tests to table tests, covering multiple cases
    now.

--- server/server_test.go
+++ server/server_test.go
@@ -23,22 +23,170 @@ func (m *MockReader) Read(p []byte) (n int, err error) {
 }
 
 func TestReadRequestLinePass(t *testing.T) {
-	value := "gemini://localhost:1965\r\n"
-	expected := "gemini://localhost:1965"
-	reader := MockReader{
-		[]byte(value),
-		0,
+
+	tests := []struct {
+		parse    string
+		expected string
+	}{
+		{
+			"\r\n",
+			"",
+		},
+
+		{
+			"gemini://localhost\r\n",
+			"gemini://localhost",
+		},
+		{
+			"gemini://localhost:1965\r\n",
+			"gemini://localhost:1965",
+		},
+		{
+			"gemini://localhost:1965/wee/woo\r\n",
+			"gemini://localhost:1965/wee/woo",
+		},
+		{
+			"gemini://localhost:1965/weeeeeeeeee\r\n",
+			"gemini://localhost:1965/weeeeeeeeee",
+		},
+		{
+			"gemini://localhost:1965/////\r\n",
+			"gemini://localhost:1965/////",
+		},
+		{
+			"gemini://localhost:1965/\r\n",
+			"gemini://localhost:1965/",
+		},
+		// Ignore after first occurrence of \r\n
+		{
+			"gemini://localhost:1965/\r\nweeewooo",
+			"gemini://localhost:1965/",
+		},
+		// Test 1024 limit
+		{
+			"gemini://localhost:1965/weeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeee\r\n",
+			"gemini://localhost:1965/weeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeee",
+		},
 	}
 
-	line, err := readRequestLine(&reader)
-	if err != nil {
-		t.Errorf("encountered unexpected error during readRequestLine")
+	for _, tt := range tests {
+		reader := MockReader{
+			[]byte(tt.parse),
+			0,
+		}
+		line, err := readRequestLine(&reader)
+		if err != nil {
+			t.Errorf("encountered unexpected error during readRequestLine, value = %v", tt.parse)
+		}
+
+		out := string(line)
+
+		if out != tt.expected {
+			t.Errorf("expected line = %v, got line = %v", tt.expected, out)
+		}
+	}
+}
+
+func TestReadRequestLineFail(t *testing.T) {
+
+	tests := []struct {
+		parse string
+	}{
+		{
+			"",
+		},
+		{
+			"\r",
+		},
+		{
+			"\n",
+		},
+		{
+			"gemini://localhost",
+		},
+		{
+			"gemini://localhost:1965",
+		},
+		{
+			"gemini://localhost:1965/wee/woo\rweewoo",
+		},
+		{
+			"gemini://localhost:1965/weeeeeeeeee\nweewwoo\r",
+		},
+		{
+			"gemini://localhost:1965/weeeeeeeeee\rweee\n",
+		},
+		// Check for request that is larger than 1026 bytes
+		{
+			"gemini://localhost:1965/weeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" +
+				"eeeeeeeeeeeeeeeeee\r\n",
+		},
 	}
 
-	out := string(line)
-
-	if out != expected {
-		t.Errorf("expected line = %v, got line = %v", expected, out)
+	for _, tt := range tests {
+		reader := MockReader{
+			[]byte(tt.parse),
+			0,
+		}
+		line, err := readRequestLine(&reader)
+		if err == nil {
+			t.Errorf("expected error, got line = %v", line)
+		}
 	}
-
 }
