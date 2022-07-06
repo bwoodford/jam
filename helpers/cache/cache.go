5d2f3d35a2b1238dd0b48d6464e8ece58d854c72
Brandon Woodford <ivegot@norto.dev>
2022-07-05 19:20:41 -0500

    Update cache with new naming conventions
    Also adding more cache unit tests.

--- helpers/cache/cache.go
+++ helpers/cache/cache.go
@@ -11,67 +11,60 @@ const MAX_SIZE int = 10
 var ErrKeyNotFound error = fmt.Errorf("key not found")
 
 type Cache struct {
-	order    list.List
-	cMap     map[string][]byte
-	cMapLock *sync.Mutex
+	queue *list.List
+	cMap  map[string][]byte
+	lock  *sync.Mutex
 }
 
 func NewCache() Cache {
 	cache := Cache{
-		cMapLock: &sync.Mutex{},
-		cMap:     make(map[string][]byte, MAX_SIZE),
-		order:    *list.New(),
+		lock:  &sync.Mutex{},
+		cMap:  make(map[string][]byte, MAX_SIZE),
+		queue: list.New(),
 	}
 	return cache
 }
 
 func (c *Cache) Remove(key string) error {
-	c.cMapLock.Lock()
+	c.lock.Lock()
 	if _, isInMap := c.find(key); !isInMap {
 		return ErrKeyNotFound
 	}
 
 	delete(c.cMap, key)
-	// start at the back - more likely to find the element
-	for e := c.order.Back(); e != nil; e = e.Prev() {
+	for e := c.queue.Front(); e != nil; e = e.Next() {
 		if e.Value == key {
-			c.order.Remove(e)
+			c.queue.Remove(e)
 		}
 	}
-	c.cMapLock.Unlock()
+	c.lock.Unlock()
 	return nil
 }
 
 func (c *Cache) Set(key string, value []byte) {
-	var keyRemoval string
+	if c.queue.Len() >= MAX_SIZE {
+		c.Remove(c.queue.Front().Value.(string))
+	}
 
-	c.cMapLock.Lock()
+	c.lock.Lock()
 
 	c.cMap[key] = value
-	c.order.PushFront(key)
+	c.queue.PushBack(key)
 
-	if len(c.cMap) > MAX_SIZE {
-		keyRemoval = c.order.Back().Value.(string)
-	}
-
-	c.cMapLock.Unlock()
-
-	if keyRemoval != "" {
-		c.Remove(keyRemoval)
-	}
+	c.lock.Unlock()
 }
 
 func (c *Cache) Get(key string) (bool, []byte) {
-	c.cMapLock.Lock()
+	c.lock.Lock()
 	val, isInMap := c.find(key)
 	if isInMap {
-		for e := c.order.Back(); e != nil; e = e.Prev() {
+		for e := c.queue.Front(); e != nil; e = e.Next() {
 			if e.Value == key {
-				c.order.MoveToFront(e)
+				c.queue.MoveToBack(e)
 			}
 		}
 	}
-	c.cMapLock.Unlock()
+	c.lock.Unlock()
 	if !isInMap {
 		return false, nil
 	}
@@ -79,10 +72,10 @@ func (c *Cache) Get(key string) (bool, []byte) {
 }
 
 func (c *Cache) Clear() {
-	c.cMapLock.Lock()
+	c.lock.Lock()
 	c.cMap = make(map[string][]byte, MAX_SIZE)
-	c.order = *c.order.Init()
-	c.cMapLock.Unlock()
+	c.queue = c.queue.Init()
+	c.lock.Unlock()
 }
 
 func (c *Cache) find(key string) (val []byte, isInMap bool) {
--- helpers/cache/cache_test.go
+++ helpers/cache/cache_test.go
@@ -1,6 +1,9 @@
 package cache
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 var global Cache
 
@@ -148,31 +151,78 @@ func TestSetPass(t *testing.T) {
 			1,
 		},
 		{
-			"SetTwoKeys",
-			[]string{
-				"Test1",
-				"Test2",
-			},
-			2,
-		},
-		{
-			"SetThreeKeys",
-			[]string{
-				"Test1",
-				"Test2",
-				"Test3",
-			},
-			3,
-		},
-		{
-			"SetFourKeys",
+			"SetFiveKeys",
 			[]string{
 				"Test1",
 				"Test2",
 				"Test3",
 				"Test4",
+				"Test5",
 			},
-			4,
+			5,
+		},
+		{
+			"SetTenKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+			},
+			MAX_SIZE,
+		},
+		{
+			"SetFifteenKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+				"Test11",
+				"Test12",
+				"Test13",
+				"Test14",
+				"Test15",
+			},
+			MAX_SIZE,
+		},
+		{
+			"SetTwentyKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+				"Test11",
+				"Test12",
+				"Test13",
+				"Test14",
+				"Test15",
+				"Test16",
+				"Test17",
+				"Test18",
+				"Test19",
+				"Test20",
+			},
+			MAX_SIZE,
 		},
 	}
 
@@ -183,6 +233,211 @@ func TestSetPass(t *testing.T) {
 		}
 		if len(global.cMap) != tc.expected {
 			t.Fatalf("%s: expected: %d got: %d", tc.desc, tc.expected, len(global.cMap))
+		}
+	}
+}
+
+func TestSetEviction(t *testing.T) {
+
+	cases := []struct {
+		desc     string
+		input    []string
+		expected []string
+	}{
+		{
+			"SetOneKey",
+			[]string{
+				"Test1",
+			},
+			[]string{
+				"Test1",
+			},
+		},
+		{
+			"SetFiveKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+			},
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+			},
+		},
+		{
+			"SetTenKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+			},
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+			},
+		},
+		{
+			"SetFifteenKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+				"Test11",
+				"Test12",
+				"Test13",
+				"Test14",
+				"Test15",
+			},
+			[]string{
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+				"Test11",
+				"Test12",
+				"Test13",
+				"Test14",
+				"Test15",
+			},
+		},
+		{
+			"SetTwentyKeys",
+			[]string{
+				"Test1",
+				"Test2",
+				"Test3",
+				"Test4",
+				"Test5",
+				"Test6",
+				"Test7",
+				"Test8",
+				"Test9",
+				"Test10",
+				"Test11",
+				"Test12",
+				"Test13",
+				"Test14",
+				"Test15",
+				"Test16",
+				"Test17",
+				"Test18",
+				"Test19",
+				"Test20",
+			},
+			[]string{
+				"Test11",
+				"Test12",
+				"Test13",
+				"Test14",
+				"Test15",
+				"Test16",
+				"Test17",
+				"Test18",
+				"Test19",
+				"Test20",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		Before()
+		for _, in := range tc.input {
+			global.Set(in, []byte{})
+		}
+
+		for _, ex := range tc.expected {
+			if _, ok := global.cMap[ex]; !ok {
+				t.Fatalf("%s: expected: %s got: ErrKeyNotFound", tc.desc, ex)
+			}
+		}
+	}
+}
+
+func TestGetPass(t *testing.T) {
+
+	cases := []struct {
+		desc  string
+		input map[string][]byte
+	}{
+		{
+			"SetOneKey",
+			map[string][]byte{
+				"Test1": []byte{'w'},
+			},
+		},
+		{
+			"SetTenKeys",
+			map[string][]byte{
+				"Test1":  []byte{'w'},
+				"Test2":  []byte{'e'},
+				"Test3":  []byte{'e'},
+				"Test4":  []byte{'w'},
+				"Test5":  []byte{'o'},
+				"Test6":  []byte{'o'},
+				"Test7":  []byte{'w'},
+				"Test8":  []byte{'a'},
+				"Test9":  []byte{'r'},
+				"Test10": []byte{'e'},
+			},
+		},
+		{
+			"SetTenMoreKeys",
+			map[string][]byte{
+				"Test6":  []byte{'o'},
+				"Test7":  []byte{'w'},
+				"Test8":  []byte{'a'},
+				"Test9":  []byte{'r'},
+				"Test10": []byte{'e'},
+				"Test11": []byte{'w'},
+				"Test12": []byte{'o'},
+				"Test13": []byte{'l'},
+				"Test14": []byte{'f'},
+				"Test15": []byte{'e'},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		Before()
+		for key, val := range tc.input {
+			global.cMap[key] = val
+			global.queue.PushBack(key)
+		}
+
+		for key, inVal := range tc.input {
+			inMap, outVal := global.Get(key)
+			if !inMap || bytes.Compare(inVal, outVal) != 0 {
+				t.Fatalf("%s: expected inMap: %v got: %v: expected val: %v got: %v", tc.desc, true, inMap, inVal, outVal)
+			}
 		}
 	}
 }
